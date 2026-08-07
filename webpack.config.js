c05b17a3eeea4606727a1b45ce44c92444b20b0f
sigizmunde <1816579@gmail.com>
2026-08-07 21:32:13 +0300

    add css import

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,36 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "main.js",
-        clean: true
+        clean: true,
     },
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./public/index.html"
-        })
+            template: "./public/index.html",
+        }),
     ],
 
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+
     devServer: {
-        static: "./dist",
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 3000,
         open: true,
-        hot: true
-    }
+        hot: true,
+        liveReload: true,
+    },
 };
